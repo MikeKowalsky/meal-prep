1eae600b2b00cfaa3e91b44b31af11b863f871a1
MikeKowalsky <mkowalewski83@gmail.com>
2018-12-16 19:43:36 +0100

    added navigation component

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
